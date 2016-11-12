04b84d27170845d3f8e22ca2435289449bf6f710
Felipe Medeiros <Felipe Medeiros>
2016-11-12 05:27:10 -0200

    Aula 2 - Tipos de Variáveis

---
+++ Class 1/app.js
@@ -0,0 +1,21 @@
+//Exemplo de declaração de variáveis
+var x = 10;
+var y = 5;
+var resultado = x + x + y * x / x;
+
+console.log(resultado);
+
+//Variaveis podem ser declaradas em conjunto
+var nome = "Felipe Medeiros",
+    idade = 21,
+    escolaridade = "Superior Incompleto";
+
+//Variáveis podem ser declaradas em branco
+var javascript; //por padrão ela irá ser setada como 'undefined'
+
+console.log(nome);
+console.log(idade);
+console.log(escolaridade);
+
+//Variáveis no javascript são casesensitive
+//Variáveis são declaradas somente uma vez, o último valor é o que prevalecerá
